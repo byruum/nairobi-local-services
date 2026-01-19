08f14df52ca84f8a2539b2d244086ce23cfe9a3e
byruum <wanyonyibyram95@gmail.com>
2026-01-19 12:19:23 +0300

    Update photos.js

--- photos.js
+++ photos.js
@@ -1,7 +1,22 @@
-const galleries = {
-  "Masai Mara": "images/masai-mara/",
-  "Nairobi National Park": "images/nairobi-national-park/",
-  "Tsavo": "images/tsavo/",
-  "Ngorongoro": "images/ngorongoro/",
-  "Samburu": "images/samburu/"
-};
+const photos = [
+  {
+    category: "Masai Mara National Reserve",
+    image: "images/masai-mara/IMG-20260111-WA0026.jpg",
+    description: "Luxury safari with the Big Five and the Great Migration."
+  },
+  {
+    category: "Nairobi National Park",
+    image: "images/nairobi-national-park/IMG-20260111-WA0033.jpg",
+    description: "Wildlife experience just outside Nairobi city."
+  },
+  {
+    category: "Lake Nakuru National Park",
+    image: "images/lake-nakuru/IMG-20260111-WA0008.jpg",
+    description: "Flamingos, rhinos and scenic lake views."
+  },
+  {
+    category: "Giraffe Centre",
+    image: "images/giraffe-centre/IMG-20260111-WA0004.jpg",
+    description: "Family-friendly interaction with giraffes."
+  }
+];
