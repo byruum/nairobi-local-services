df19c0dc7a3aa2a1a831eb02c1ef0d773d45edee
byruum <wanyonyibyram95@gmail.com>
2026-01-19 11:56:26 +0300

    Update photos.js

--- photos.js
+++ photos.js
@@ -1,1 +1,7 @@
-const photos = [];
+const galleries = {
+  "Masai Mara": "images/masai-mara/",
+  "Nairobi National Park": "images/nairobi-national-park/",
+  "Tsavo": "images/tsavo/",
+  "Ngorongoro": "images/ngorongoro/",
+  "Samburu": "images/samburu/"
+};
